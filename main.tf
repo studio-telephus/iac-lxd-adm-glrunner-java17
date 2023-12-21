c0be57ca0a18b8029f018383d80767f680f91713
Kristo Aun <kristoaun@live.com>
2023-12-21 14:34:27 +0200

    run

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "container_glrunner_java17" {
-  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=main"
-  name      = "container-glrunner-java17"
+module "container_adm_glrunner_java17" {
+  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=1.0.1"
+  name      = "container-adm-glrunner-java17"
   image     = "images:debian/bookworm"
   profiles  = ["limits", "fs-dir", "nw-adm"]
-  autostart = true
+  autostart = false
   nic = {
     name = "eth0"
     properties = {
@@ -17,13 +17,11 @@ module "container_glrunner_java17" {
     "${path.cwd}/filesystem",
   ]
   exec_enabled = true
-  exec = [{
-    entrypoint = "/mnt/install.sh"
-    environment = {
-      RANDOM_STRING                  = "d9c927f6-1463-4aaa-a2cf-37e7e2b281e1"
-      GITLAB_RUNNER_REGISTRATION_KEY = var.gitlab_runner_registration_key
-      GIT_SA_USERNAME                = var.git_sa_username
-      GIT_SA_TOKEN                   = var.git_sa_token
-    }
-  }]
+  exec         = "/mnt/install.sh"
+  environment = {
+    RANDOM_STRING                  = "d9c927f6-1463-4aaa-a2cf-37e7e2b281e1"
+    GITLAB_RUNNER_REGISTRATION_KEY = var.gitlab_runner_registration_key
+    GIT_SA_USERNAME                = var.git_sa_username
+    GIT_SA_TOKEN                   = var.git_sa_token
+  }
 }
